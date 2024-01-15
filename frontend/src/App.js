f12fd4f28589ff187a435c1d255314331abe1195
AbhiShirke1 <abhishekshirke263@gmail.com>
2024-01-15 18:26:34 +0530

    major project

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navbar />} >
+            <Route index element={<Home />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
