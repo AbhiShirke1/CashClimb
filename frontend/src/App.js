344c887a0bd33ccf917d979d2267066f0f4f6c8a
Sandeep Sinha <sinhasandy345@gmail.com>
2024-01-18 16:08:59 +0530

    Main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer/Footer';
+import Signup from './components/auth/Sigup/Signup';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Navbar />} >
+          <Route path='/' element={<Footer />} >
             <Route index element={<Home />}/>
           </Route>
+          <Route path='/signup' element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
     </>
