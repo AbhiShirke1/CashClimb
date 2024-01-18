30e6e2e6909dd7b72331abf02837ec231c5548d7
AbhiShirke1 <abhishekshirke263@gmail.com>
2024-01-18 22:50:26 +0530

    minor changes when sandeep merged to main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {io} from 'socket.io-client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar/Navbar';
+import Signup from './components/auth/Sigup/Signup';
 
 function App() {
   return (
